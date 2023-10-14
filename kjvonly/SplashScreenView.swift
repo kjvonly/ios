5b74fb7f8b8a813316a9fe1242b0293598e7b4de
man4christ <man4christ.65qa8@simplelogin.com>
2023-10-14 14:48:35 -0400

    * Creates template for splash screen * Adds in app icon svg and png.

---
+++ kjvonly/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  kjvonly
+//
+//  Created by man4christ on 10/14/23.
+//
+
+import Foundation
+import SwiftUI
+
+// Resources https://youtu.be/0ytO3wCRKZU
+// SplashScreen for iOS in SwiftUI Tutorial 2022 (Xcode) chan: Indently
+
+struct SplashScreenView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
