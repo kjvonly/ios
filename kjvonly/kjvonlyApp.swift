3a26dbb2765ae7e856c8b9b84dd8af9f4103ea2d
man4christ <man4christ.65qa8@simplelogin.com>
2023-10-14 15:34:44 -0400

    * Adds splash screen with icon * Adds animation to splash screen

--- kjvonly/kjvonlyApp.swift
+++ kjvonly/kjvonlyApp.swift
@@ -10,15 +10,12 @@ import SwiftData
 
 @main
 struct kjvonlyApp: App {
-    let persistenceController = PersistenceController.shared
     
     var body: some Scene {
         WindowGroup {
-            let bdao: BibleDao = BibleDao()
-            let br: BibleRepository = BibleRepository(bibleDao: bdao)
-            let bvm:            BibleViewModel = BibleViewModel(bibleRepository: br)
-            BibleView(vm: bvm)
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+
+            SplashScreenView()
+           
         }
     }
 }
